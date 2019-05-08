183296b962a159f08c071c8378a8df920e5edb71
Young Liu <youngliu92@gmail.com>
2019-05-08 14:19:53 -0700

    add own custom middleware.

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { createStore, applyMiddleware } from "redux";
+import middlewareLogger from "./middleware/middleware-logger";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(middlewareLogger));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('react-app-root')
+  document.getElementById("react-app-root")
 );
